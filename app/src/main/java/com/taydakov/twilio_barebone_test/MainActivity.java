5a0977f0cb93f2573c7deddaa0fc543dab00c7c5
Lev Taydakov <lev.taydakov@gmail.com>
2016-05-11 16:28:54 -0700

    Refactor

--- app/src/main/java/com/taydakov/twilio_barebone_test/MainActivity.java
+++ app/src/main/java/com/taydakov/twilio_barebone_test/MainActivity.java
@@ -18,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
         /* Test Twilio Chat API */
         Log.d(TAG, "Chat initialization...");
-        chatAPI = new OldTwilioChatApi(this, "eyJ0eXAiOiAiSldUIiwgImN0eSI6ICJ0d2lsaW8tZnBhO3Y9MSIsICJhbGciOiAiSFMyNTYifQ.eyJncmFudHMiOiB7ImlkZW50aXR5IjogNCwgImlwX21lc3NhZ2luZyI6IHsiZW5kcG9pbnRfaWQiOiAiT25lVW5pQXBwOjQ6bW9iaWxldGVzdCIsICJzZXJ2aWNlX3NpZCI6ICJJU2ZiOGZiOWYxMTNjMTRjMGFhNDg2MGYxM2I2NmU2ZWEzIn19LCAic3ViIjogIkFDMGQ0NzA2MjJkNjc1MGQ3ZDJiM2NkZWEyMmFiMTI0OGEiLCAianRpIjogIlNLMGIxNDRjYmIzMjIyNmExNzkwYTJlZGMxMGY3MTRjOTYtMTQ2MjgxOTA2MCIsICJleHAiOiAxNDYyOTAxODYwLCAiaXNzIjogIlNLMGIxNDRjYmIzMjIyNmExNzkwYTJlZGMxMGY3MTRjOTYifQ.8dZEKNmdJqFMZY1fqAt8BZLvwIUyC_d4PaJzbjDAkaU");
+        // test old API: chatAPI = new OldTwilioChatApi(this, ""); // TODO: put Twilio token
+        chatAPI = new OldTwilioChatApi(this, "eyJ0eXAiOiAiSldUIiwgImN0eSI6ICJ0d2lsaW8tZnBhO3Y9MSIsICJhbGciOiAiSFMyNTYifQ.eyJncmFudHMiOiB7ImlkZW50aXR5IjogNCwgImlwX21lc3NhZ2luZyI6IHsiZW5kcG9pbnRfaWQiOiAiT25lVW5pQXBwOjQ6bW9iaWxldGVzdCIsICJzZXJ2aWNlX3NpZCI6ICJJU2ZiOGZiOWYxMTNjMTRjMGFhNDg2MGYxM2I2NmU2ZWEzIn19LCAic3ViIjogIkFDMGQ0NzA2MjJkNjc1MGQ3ZDJiM2NkZWEyMmFiMTI0OGEiLCAianRpIjogIlNLMGIxNDRjYmIzMjIyNmExNzkwYTJlZGMxMGY3MTRjOTYtMTQ2MjgxOTA2MCIsICJleHAiOiAxNDYyOTAxODYwLCAiaXNzIjogIlNLMGIxNDRjYmIzMjIyNmExNzkwYTJlZGMxMGY3MTRjOTYifQ.8dZEKNmdJqFMZY1fqAt8BZLvwIUyC_d4PaJzbjDAkaU"); // TODO: put Twilio token
         chatAPI.initialize(new IChatEventListener() {
             @Override
             public void onSuccess() {
